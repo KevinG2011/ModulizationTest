a360d3d5985f0f24abf2325143602201307ff37f
lijia <lijia@huajiao.tv>
2019-02-01 11:13:13 +0800

    reinit project

---
+++ ModulizationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModulizationTest
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModulizationTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ModulizationTest
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
